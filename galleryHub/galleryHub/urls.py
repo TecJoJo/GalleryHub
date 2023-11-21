f6a9738b2568db4475936146ca3eb7946a56e2f5
Yao Lu laptop <luyao2493751@gmail.com>
2023-11-21 18:20:21 +0200

    project rename

--- galleryHub/galleryHub/urls.py
+++ galleryHub/galleryHub/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for galleryApp project.
+URL configuration for galleryHub project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
---
+++ galleryHub/galleryHub_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GalleryhubApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'galleryHub_api'
